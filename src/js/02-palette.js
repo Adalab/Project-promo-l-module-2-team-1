8e6f326bdd566f257b4e9f9886c3fbd75a622fac
Carmen Ramos <carmen.rama91@gmail.com>
2021-02-01 21:13:38 +0100

    add palette styles in css to change in photocard

--- src/js/02-palette.js
+++ src/js/02-palette.js
@@ -4,10 +4,10 @@ const paletteElements = document.querySelectorAll('.js-palette');
 const photoCardElement = document.querySelector('.js-card');
 
 function handlePalette() {
-    photoCardElement.classList.remove('palette-1', 'palette-2', 'palette-3');
+    photoCardElement.classList.remove('palette-1', 'palette-2', 'palette-3', 'palette-4');
     const checkedPalette = document.querySelector('.js-palette:checked');
     const checkedPaletteValue = checkedPalette.value;
-    photoCardElement.classList.add('palette- ' + checkedPaletteValue);
+    photoCardElement.classList.add('palette-' + checkedPaletteValue);
 }
 
 for (const onePalette of paletteElements) {
