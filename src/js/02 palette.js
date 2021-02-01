f7036560f4cd4f111c8b37f36f9394b75566b453
CCrisBIB <cristinabiglesias@gmail.com>
2021-02-01 20:22:40 +0100

    Add and fill 02 palette.js to change color in photocard when change option

---
+++ src/js/02 palette.js
@@ -0,0 +1,17 @@
+'use.strict';
+
+const paletteElements = document.querySelectorAll('.js-palette');
+const photoCardElement = document.querySelector('.js-card');
+
+function handlePalette() {
+    photoCardElement.classList.remove('palette-1', 'palette-2', 'palette-3');
+    const checkedPalette = document.querySelector('.js-palette:checked');
+    const checkedPaletteValue = checkedPalette.value;
+    photoCardElement.classList.add('palette- ' + checkedPaletteValue);
+}
+
+for (const onePalette of paletteElements) {
+onePalette.addEventListener('change', handlePalette);
+}
+
+handlePalette();
