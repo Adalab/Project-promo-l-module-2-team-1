7ec24618cb336900d58e3e33953031a1466fda59
Marta2Ges <ggarridomarta@gmail.com>
2021-02-02 13:05:22 +0100

    Add hidden share and fill

--- src/js/01-collapsable.js
+++ src/js/01-collapsable.js
@@ -1,14 +1,47 @@
 `use strict`;
 
-
 //Traer el elemento del html//
-
-const designCollapsableHeader=document.querySelector (".js-collapsable-header"); 
-const designCollapsableContainer = document.querySelector('.js-collapsable-container')
-
-function handleCollapsable() {
-    console.log('Me han clickado');
-    designCollapsableContainer.classList.toggle('collapsable--close')
+debugger;
+const collapsableHeaders = document.querySelectorAll(".js-collapsable-header");
+for (const collapsableHeader of collapsableHeaders) {
+  collapsableHeader.addEventListener("click", changeCollapsable);
 }
 
-designCollapsableHeader.addEventListener('click', handleCollapsable);
+function changeCollapsable(ev) {
+  // cojo la cabecera pulsada
+  // - la cabecera es currentTarget porque estoy haciendo addEventListener sobre las cabeceras
+  const clickedHeader = ev.currentTarget;
+  // cojo el collapsable pulsado, que es nodo o etiqueta padre de la cabecera pulsada
+  const clickedCollapsable = clickedHeader.parentNode;
+  // esto también podemos hacerlo con https://developer.mozilla.org/es/docs/Web/API/Element/closest
+
+  // recorro todos los collapsables
+  const allCollapsables = document.querySelectorAll(
+    ".js-collapsable-container"
+  );
+
+  for (const collapsable of allCollapsables) {
+    // compruebo si es el collapsable pulsado
+    if (clickedCollapsable === collapsable) {
+      // si es el pulsado, le hago un toggle
+      collapsable.classList.toggle("collapsable--close");
+    } else {
+      // si no es el pulsado, lo cierro
+      collapsable.classList.add("collapsable--close");
+    }
+  }
+}
+
+const designCollapsableContainer = document.querySelectorAll(
+  ".js-collapsable-container"
+);
+
+function handleCollapsable(ev) {
+  console.log("Me han clickado");
+  designCollapsableContainer.classList.toggle("collapsable--close");
+
+  ev.currentTarget.parentNode.querySelector(".js-container");
+}
+for (const collapsableElements of designCollapsableContainer) {
+  collapsableElements.addEventListener("click", handleCollapsable);
+}
