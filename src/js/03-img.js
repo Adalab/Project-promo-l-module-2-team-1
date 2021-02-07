ea2e41dcfe4aad2eb9780dc7977869c8f782ffe3
Carmen Ramos <carmen.rama91@gmail.com>
2021-02-07 22:42:29 +0100

    solve problem fixes

--- src/js/03-img.js
+++ src/js/03-img.js
@@ -1,12 +1,12 @@
-'use strict';
+"use strict";
 
 const fr = new FileReader();
-const uploadBtn = document.querySelector('.js__profile-trigger');
-const fileField = document.querySelector('.js__profile-upload-btn');
-const profileImage = document.querySelector('.js__profile-image');
-const profilePreview = document.querySelector('.js__profile-preview');
+const uploadBtn = document.querySelector(".js__profile-trigger");
+const fileField = document.querySelector(".js__profile-upload-btn");
+const profileImage = document.querySelector(".js__profile-image");
+const profilePreview = document.querySelector(".js__profile-preview");
 
-let photo = '';
+let photo = "";
 
 /**
  * Recoge el archivo añadido al campo de tipo "file"
@@ -17,9 +17,9 @@ let photo = '';
  * @param {evento} e
  */
 function getImage(e) {
-    e.preventDefault();
+  e.preventDefault();
   const myFile = e.currentTarget.files[0];
-  fr.addEventListener('load', writeImage);
+  fr.addEventListener("load", writeImage);
   fr.readAsDataURL(myFile);
 }
 
@@ -34,16 +34,15 @@ function writeImage() {
    * de nuestro componente.
    */
 }
-  console.log(fr.result);
-  photo = fr.result;
-  function updatePhoto() {
-    const currentPhoto = photo || 'url(../images/photo-preview1.jpg)';
-    profilePreview.style.backgroundImage = `url(${currentPhoto})`;
-    profileImage.style.backgroundImage = `url(${currentPhoto})`;
-  }
+console.log(fr.result);
+photo = fr.result;
+function updatePhoto() {
+  const currentPhoto = photo || "url(../images/photo-preview1.jpg)";
+  profilePreview.style.backgroundImage = `url(${currentPhoto})`;
+  profileImage.style.backgroundImage = `url(${currentPhoto})`;
+  //meter un saveInLocalStorage??? PREGUNTAR
+}
 
-
-  
 /**
  * Genera un click automático en nuesto campo de tipo "file"
  * que está oculto
@@ -58,5 +57,5 @@ function fakeFileClick(ev) {
  * - al botón visible para generar el click automático
  * - al campo oculto para cuando cambie su value
  */
-uploadBtn.addEventListener('click', fakeFileClick);
-fileField.addEventListener('change', getImage);
+uploadBtn.addEventListener("click", fakeFileClick);
+fileField.addEventListener("change", getImage);
