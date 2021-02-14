06c614642116df5e847e46dcb87f77b282a4a0cf
Carmen Ramos <carmen.rama91@gmail.com>
2021-02-15 00:48:09 +0100

    add transition final commit

--- src/js/09-main.js
+++ src/js/09-main.js
@@ -1,9 +1,3 @@
 "use strict";
 
 getFromLocalStorage();
-
-//A priori, no es necesario invocarlas de nuevo al final, porque ya quedan invocadas cuando comienza a correr getFromLocalStorage()
-/*  updateAllInputs();
-        updatePalette();
-        updatePhoto(); 
-    */
